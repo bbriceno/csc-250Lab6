47391886920734cdc3f2abc3e5136271ec391196
bbriceno <42550883+bbriceno@users.noreply.github.com>
2019-05-03 10:02:35 -0500

    Create lab6-p4-sortedLinkedCards.c

---
+++ lab6-p4-sortedLinkedCards.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	
+#include "myFunctionsLib.h"
+
+int main(int argc, char *argv[]) {
+
+	mtgCardType *topOfCardList;
+	
+	
+	printf("Please Enter a Title, Spell Type, Mana Cost, Power, Toughness. Values should be comma separated. \n");
+	printf("Press enter key twice to end card input.\n");
+	
+    //load cards
+	topOfCardList = loadCardList(); 
+    
+    //call to function to sort cards
+    topOfCardList = sortCardsByTitle(topOfCardList);
+	
+    //call print cards function
+	printMtgCardList(topOfCardList);	
+
+	return 0;
+}
